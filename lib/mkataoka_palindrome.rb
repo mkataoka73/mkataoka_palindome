d6fe8e423e8747ebf3d6f5a1f2d6f97928839ce4
Masayoshi Kataoka <mkataoka73@gmail.com>
2019-11-12 10:21:57 +0900

    Initialize repository

---
+++ lib/mkataoka_palindrome.rb
@@ -0,0 +1,6 @@
+require "mkataoka_palindrome/version"
+
+module MkataokaPalindrome
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/mkataoka_palindrome_test.rb
@@ -0,0 +1,11 @@
+require "test_helper"
+
+class MkataokaPalindromeTest < Minitest::Test
+  def test_that_it_has_a_version_number
+    refute_nil ::MkataokaPalindrome::VERSION
+  end
+
+  def test_it_does_something_useful
+    assert false
+  end
+end
