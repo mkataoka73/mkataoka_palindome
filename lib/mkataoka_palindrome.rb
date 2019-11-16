2c4824d85ffa49a002b32f494a2ecabcc07d8226
Masayoshi Kataoka <mkataoka73@gmail.com>
2019-11-17 00:39:51 +0900

    Factor into module, add integer palindrome detection

--- lib/mkataoka_palindrome.rb
+++ lib/mkataoka_palindrome.rb
@@ -1,22 +1,23 @@
 require "mkataoka_palindrome/version"
 
-class String
-
+module MkataokaPalindrome
   # Returns true for a palindrome, false otherwise.
   def palindrome?
     processed_content == processed_content.reverse
-  end
-
-  # Returns the letters in the string.
-
-  def letters
-    self.scan(/[a-z]/i).join
   end
 
   private
 
     # Returns content for palindrome testing.
     def processed_content
-      self.scan(/[a-z]/i).join.downcase
+      self.to_s.scan(/[a-z\d]/i).join.downcase
     end
+end
+
+class String
+  include MkataokaPalindrome
+end
+
+class Integer
+  include MkataokaPalindrome
 end
